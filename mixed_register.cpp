32a21b45ff57f92fad08c9b1234a9a8dcf8c638e
kekoveca <kulanov.av@phystech.edu>
2022-04-22 15:08:03 +0300

    Доделана пятая

--- mixed_register.cpp
+++ mixed_register.cpp
@@ -6,32 +6,23 @@
 
 using namespace std;
 
-// bool caseInsCharCompareN(char a, char b) {
-//     return(toupper(a) == toupper(b));
-// }
+struct int_str {
+    unsigned long long a;
+    string b;
+};
 
-// bool caseInsCompare(const string& s1, const string& s2) {
-//     return((s1.size() == s2.size()) && equal(s1.begin(), s1.end(), s2.begin(), caseInsCharCompareN));
-// }
-
-// bool operator == (const string& lhs, const string& rhs) {
-//     return caseInsCompare(lhs, rhs);
-// }
-
-// struct Compare
-// {
-//     bool operator()(const string& lhs, const string& rhs) const
-//     {
-//         if (caseInsCompare(lhs, rhs))
-//         {
-//             return true;
-//         }
-//         else 
-//         {
-//             return lhs < rhs;
-//         }
-//     }
-// };
+class Compare{
+public:
+    bool operator()(const int_str& lhs, const int_str& rhs) const
+    {
+        if (lhs.a == rhs.a)
+            return lhs.b < rhs.b;
+        else
+        {
+            return lhs.a > rhs.a;
+        }
+    }
+};
 
 int main(){
 
@@ -43,20 +34,18 @@ int main(){
     for(unsigned i = 0; i < N; i++){
         string original;
         cin >> original;
-        string lower_reg;
+        string lower_reg = original;
         transform(original.begin(), original.end(), lower_reg.begin(), ::tolower);
         str_set[lower_reg].insert(original);
     }
+
+    set<int_str, Compare> s;
+
+    for (auto& it : str_set) {
+        s.insert(int_str{ it.second.size(), it.first });
+    }
     
-    map <int, string> int_str;
-
-    for(auto& el: str_set){
-        auto tmp_size = el.second.size();
-        int_str[tmp_size] = el.first;
-    }
-
-    for(auto& el: int_str){
-        if(true)
-            cout << el.first << " is " << el.second << endl;
-    }
+    for (auto& it : s)
+        if(it.a > 2)
+            cout << it.b << endl;
 }
