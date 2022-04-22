1c4145cfda276de42599b94b6abed5712d4b1711
kekoveca <kulanov.av@phystech.edu>
2022-04-22 11:58:43 +0300

    Смешанный регистр, не работает

--- mixed_register.cpp
+++ mixed_register.cpp
@@ -2,48 +2,61 @@
 #include <algorithm>
 #include <map>
 #include <string>
+#include <set>
 
 using namespace std;
 
-bool caseInsCharCompareN(char a, char b) {
-    return(toupper(a) == toupper(b));
-}
+// bool caseInsCharCompareN(char a, char b) {
+//     return(toupper(a) == toupper(b));
+// }
 
-bool caseInsCompare(const string& s1, const string& s2) {
-    return((s1.size() == s2.size()) && equal(s1.begin(), s1.end(), s2.begin(), caseInsCharCompareN));
-}
+// bool caseInsCompare(const string& s1, const string& s2) {
+//     return((s1.size() == s2.size()) && equal(s1.begin(), s1.end(), s2.begin(), caseInsCharCompareN));
+// }
 
-struct Compare
-{
-    bool operator()(const string& lhs, const string& rhs) const
-    {
-        if (caseInsCompare(lhs, rhs))
-        {
-            return true;
-        }
-        else 
-        {
-            return lhs < rhs;
-        }
-    }
-};
+// bool operator == (const string& lhs, const string& rhs) {
+//     return caseInsCompare(lhs, rhs);
+// }
+
+// struct Compare
+// {
+//     bool operator()(const string& lhs, const string& rhs) const
+//     {
+//         if (caseInsCompare(lhs, rhs))
+//         {
+//             return true;
+//         }
+//         else 
+//         {
+//             return lhs < rhs;
+//         }
+//     }
+// };
 
 int main(){
+
     unsigned N;
     cin >> N;
 
-    map <string, int, Compare> m;
+    map <string, set<string>> str_set;
 
     for(unsigned i = 0; i < N; i++){
-        string tmp;
-        cin >> tmp;
-        // transform(tmp.begin(), tmp.end(), tmp.begin(), ::tolower);
-        m[tmp]++;
+        string original;
+        cin >> original;
+        string lower_reg;
+        transform(original.begin(), original.end(), lower_reg.begin(), ::tolower);
+        str_set[lower_reg].insert(original);
+    }
+    
+    map <int, string> int_str;
+
+    for(auto& el: str_set){
+        auto tmp_size = el.second.size();
+        int_str[tmp_size] = el.first;
     }
 
-    for(auto& el: m){
+    for(auto& el: int_str){
         if(true)
             cout << el.first << " is " << el.second << endl;
     }
-
 }
