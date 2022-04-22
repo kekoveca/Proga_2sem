967e55a90bd79bf7b4f2a76568c89ddc1c2bac93
kekoveca <kulanov.av@phystech.edu>
2022-04-22 10:55:15 +0300

    Проблема кастомного компаратора

--- mixed_register.cpp
+++ mixed_register.cpp
@@ -9,32 +9,40 @@ bool caseInsCharCompareN(char a, char b) {
     return(toupper(a) == toupper(b));
 }
 
-string toLower(string& s){
-    for(auto& el: s){
-        el = tolower(el);
-    }
-}
-
 bool caseInsCompare(const string& s1, const string& s2) {
     return((s1.size() == s2.size()) && equal(s1.begin(), s1.end(), s2.begin(), caseInsCharCompareN));
 }
 
+struct Compare
+{
+    bool operator()(const string& lhs, const string& rhs) const
+    {
+        if (caseInsCompare(lhs, rhs))
+        {
+            return true;
+        }
+        else 
+        {
+            return lhs < rhs;
+        }
+    }
+};
 
 int main(){
     unsigned N;
     cin >> N;
 
-    map <string, int> m;
+    map <string, int, Compare> m;
 
     for(unsigned i = 0; i < N; i++){
         string tmp;
         cin >> tmp;
-        transform(tmp.begin(), tmp.end(), tmp.begin(), ::tolower);
+        // transform(tmp.begin(), tmp.end(), tmp.begin(), ::tolower);
         m[tmp]++;
     }
 
     for(auto& el: m){
-        if(el.second != 1)
+        if(true)
             cout << el.first << " is " << el.second << endl;
     }
 
